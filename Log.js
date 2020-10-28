e69980e097b4d621c0430ec146f13025d1d466ed
ujasbatheja <71878150+ujasbatheja@users.noreply.github.com>
2020-10-28 17:43:28 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -2,7 +2,7 @@ class Log extends BaseClass{
     constructor(x, y, height, angle) {
       super(x,y,20,height,angle);
       Matter.Body.setAngle(this.body, angle);
-      this.image=loadImage("sprites/wood2.png");
+      this.image=loadImage("wood2.png");
     }
   };
   
